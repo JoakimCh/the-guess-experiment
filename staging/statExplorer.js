741fe839933b3b10e6570bdf70c78aebe2ff1aa8
JoakimCh <5984627+JoakimCh@users.noreply.github.com>
2025-03-12 13:02:27 +0100

    .

--- staging/statExplorer.js
+++ staging/statExplorer.js
@@ -7,9 +7,50 @@ export const uiContainer = e.div.class('mainContainer')(
   e.h1('The Guess Experiment'),
   e.h2('Stat explorer'),
   e.p('Not implemented yet...'),
+  e.p('For now you can download the database and analyze it yourself.'),
+  ui.button_export = e.button('Export DB to JSON'),
   ui.button_back = e.button('Back to the experiment')
 )
 
-// ui.button_back.onclick = () => {
+ui.button_export.onclick = () => {
+  exportIndexedDBToJSON('stats', 'theGuessExperimentLocalDatabase')
+}
 
-// }
+function exportIndexedDBToJSON(dbName, exportName = dbName) {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open(dbName)
+    request.onsuccess = (event) => {
+      const db = event.target.result
+      const data = {}
+      const tx = db.transaction(db.objectStoreNames, "readonly")
+      for (const storeName of db.objectStoreNames) {
+        const store = tx.objectStore(storeName)
+        data[storeName] = []
+        const cursorRequest = store.openCursor()
+        cursorRequest.onsuccess = (e) => {
+          const cursor = e.target.result
+          if (cursor) {
+            data[storeName].push({
+              key: cursor.key,
+              value: cursor.value
+            })
+            cursor.continue()
+          }
+        }
+        cursorRequest.onerror = () => reject(`Cursor error in ${storeName}`)
+      }
+      tx.oncomplete = () => {
+        const json = JSON.stringify(data, null, 2)
+        resolve(json)
+        const blob = new Blob([json], { type: "application/json" })
+        const url = URL.createObjectURL(blob)
+        const a = document.createElement("a")
+        a.href = url
+        a.download = `${exportName}.json`
+        a.click()
+      }
+      tx.onerror = () => reject("Transaction error")
+    }
+    request.onerror = () => reject("Database error")
+  })
+}
